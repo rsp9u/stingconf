dcde818e00df17f952622e40a86442b184268ec3
rsp9u <gctwt852@yahoo.co.jp>
2019-04-05 14:04:09 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='stingconf',
+    version='0.0.1',
+    author='',
+    author_email='',
+    maintainer='rsp9u',
+    maintainer_email='gctwt852@yahoo.co.jp',
+    license='',
+    url='',
+    description='Layered configuration library',
+    packages=['stingconf'],
+    install_requires=[
+        'PyYAML',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+    ],
+)
