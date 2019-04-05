8b76c413189988d7d14580347a14aad0f397f3d7
rsp9u <gctwt852@yahoo.co.jp>
2019-04-05 23:36:01 +0900

    Add tests for parsing with definitions

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -223,3 +223,46 @@ def test_file_json(tmpdir):
     parser.conf_file(p.strpath, type='json')
     config = parser.parse()
     assert config.FOO_CONFIG == 'foo'
+
+
+def _definitions():
+    return {
+        'items': {
+            'foo-config': {
+                'default': 5,
+                'type': 'int',
+            },
+            'bar-config': {
+                'default': 0,
+                'type': 'int',
+            },
+        }
+    }
+
+
+def test_parse_with_definitions():
+    os.environ['BAR_CONFIG'] = '10'
+
+    parser = stingconf.Parser('Test module', _definitions())
+    config = parser.parse()
+    assert config.FOO_CONFIG == 5
+    assert config.BAR_CONFIG == 10
+
+    os.environ.pop('BAR_CONFIG')
+
+
+def test_parse_with_definitions_twice():
+    os.environ['BAR_CONFIG'] = '10'
+    d = _definitions()
+
+    parser = stingconf.Parser('Test module', d)
+    config = parser.parse()
+    assert config.FOO_CONFIG == 5
+    assert config.BAR_CONFIG == 10
+
+    parser = stingconf.Parser('Test module', d)
+    config = parser.parse()
+    assert config.FOO_CONFIG == 5
+    assert config.BAR_CONFIG == 10
+
+    os.environ.pop('BAR_CONFIG')
