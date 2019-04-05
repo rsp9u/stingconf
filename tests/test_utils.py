7f12942780554de7f70bf760f343f24d22fbd8cc
rsp9u <gctwt852@yahoo.co.jp>
2019-04-05 17:38:47 +0900

    Make unit tests parametrized

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,26 +1,23 @@
+import pytest
 from stingconf import utils
 
 
-def test_str_to_type():
-    type_map = {
-        'str': (0, '0'),
-        'int': ('0', 0),
-        'float': ('0.1', 0.1),
-        'bool': ('True', True),
-    }
-
-    for type_str, (input, expected) in type_map.items():
-        cast_func = utils.str_to_type(type_str)
-        assert expected == cast_func(input)
+@pytest.mark.parametrize('type_str,test_input,expected', [
+    ('str', 0, '0'),
+    ('int', '0', 0),
+    ('float', '0.1', 0.1),
+    ('bool', 'True', True),
+])
+def test_str_to_type(type_str, test_input, expected):
+    cast_func = utils.str_to_type(type_str)
+    assert expected == cast_func(test_input)
 
 
-def test_to_upper_snake():
-    strings = [
-        ('spi-nal-case', 'SPI_NAL_CASE'),
-        ('mixed-CASE', 'MIXED_CASE'),
-        ('-prefix', '_PREFIX'),
-        ('postfix-', 'POSTFIX_'),
-    ]
-
-    for (input, expected) in strings:
-        assert expected == utils.to_upper_snake(input)
+@pytest.mark.parametrize('test_input,expected', [
+    ('spi-nal-case', 'SPI_NAL_CASE'),
+    ('mixed-CASE', 'MIXED_CASE'),
+    ('-prefix', '_PREFIX'),
+    ('postfix-', 'POSTFIX_'),
+])
+def test_to_upper_snake(test_input, expected):
+    assert expected == utils.to_upper_snake(test_input)
