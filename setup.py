bb5ce362107fc652bfee2e9601b4f5d02173f9f2
rsp9u <gctwt852@yahoo.co.jp>
2019-04-05 21:13:18 +0900

    Update setup.py for registering to PyPI

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import codecs
 from setuptools import setup
 
 
@@ -10,6 +12,12 @@ tests_require = [
 if sys.version_info < (3, 0):
     tests_require.append('mock')
 
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
 setup(
     name='stingconf',
     version='0.0.1',
@@ -18,8 +26,11 @@ setup(
     maintainer='rsp9u',
     maintainer_email='gctwt852@yahoo.co.jp',
     license='MIT',
-    url='',
+    url='https://github.com/rsp9u/stingconf',
     description='Layered configuration library',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    zip_safe=False,
     packages=['stingconf'],
     install_requires=[
         'PyYAML',
@@ -29,8 +40,12 @@ setup(
     ],
     tests_require=tests_require,
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
     ],
 )
