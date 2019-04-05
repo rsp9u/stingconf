fd685f1e76e0fb1d68edaeae270b1b919386fbb2
rsp9u <gctwt852@yahoo.co.jp>
2019-04-05 23:01:07 +0900

    Add functions in order to list all items into the parsed result class

--- stingconf/config.py
+++ stingconf/config.py
@@ -4,8 +4,22 @@ class ConfigMeta():
 
 class Config():
     def __init__(self):
+        self._keys = []
         self.meta = ConfigMeta()
 
     def add(self, name, value, meta):
+        self._keys.append(name)
         setattr(self, name, value)
         setattr(self.meta, name, meta)
+
+    def keys(self):
+        for k in self._keys:
+            yield k
+
+    def values(self):
+        for k in self._keys:
+            yield getattr(self, k)
+
+    def items(self):
+        for k in self._keys:
+            yield k, getattr(self, k)
